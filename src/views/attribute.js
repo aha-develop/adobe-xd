b815ca70c0099fbbe43a6990f2868d6cc1938618
Jeffery Tucker <jeff@aha.io>
2022-02-09 17:10:16 -0500

    Create Adobe XD extension

---
+++ src/views/attribute.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { EmbeddedContentAttribute } from "@aha-app/aha-develop-react";
+
+aha.on("adobeXdAttribute", ({ record, fields }, { identifier }) => {
+  return (
+    <EmbeddedContentAttribute
+      identifier={identifier}
+      record={record}
+      fields={fields}
+      product="Adobe XD"
+      placeholder="https://xd.adobe.com/embed/..."
+    />
+  );
+});
