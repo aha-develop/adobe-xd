6d630bdccd1897c0dca991c28410438d87462163
Jeffery Tucker <jeff@aha.io>
2022-02-24 13:34:55 -0500

    Switch to @aha-develop/aha-develop-react

--- src/views/attribute.js
+++ src/views/attribute.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { EmbeddedContentAttribute } from "@aha-app/aha-develop-react";
+import { EmbeddedContentAttribute } from "@aha-develop/aha-develop-react";
 
 aha.on("adobeXdAttribute", ({ record, fields }, { identifier }) => {
   return (
